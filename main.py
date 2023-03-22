87a92f5dbd2cbb9833ce849ba81fa928fce0fcce
Keython <keita.laimina@gmail.com>
2023-03-22 23:18:50 +0200

    ta

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 
 def parallel_processing(n, m, data):
     output = []
+    th=[0]*n
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
+    for i in range(m):
+        minth=min(th)
+        th[th.index(minth)]= th[th.index(minth)]+data[i]
 
     return output
 
@@ -15,17 +19,14 @@ def main():
     # m - job count
     n = 0
     m = 0
-
+    n,m =map(int,input().split())
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
     data = []
-
     # TODO: create the function
     result = parallel_processing(n,m,data)
-    
     # TODO: print out the results, each pair in it's own line
-
-
-
+    for i,k in result:
+        print(i, k)
 if __name__ == "__main__":
     main()
